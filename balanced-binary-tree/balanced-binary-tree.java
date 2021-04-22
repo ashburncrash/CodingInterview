cf96f271e59025aa93beaa894c8bedf13dc1962b
ashburncrash <ashburncrash@gmail.com>
2021-04-22 09:15:04 +0200

    Time: 0 ms (100.00%), Space: 38.8 MB (75.81%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.java
+++ balanced-binary-tree/balanced-binary-tree.java
@@ -14,15 +14,35 @@
  * }
  */
 class Solution {
+    // top-down approach; calc depth till the leaves -> o(n^2)
+//     public boolean isBalanced(TreeNode root) {
+//         if (root == null) return true;
+        
+//         return Math.abs(calcDepth(root.left)  - calcDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+//     }
+    
+//     public int calcDepth(TreeNode root) {
+//         if (root == null) return 0;
+        
+//         return Math.max(calcDepth(root.left) + 1, calcDepth(root.right) + 1);
+//     }
+    
+    // bottom top approach
+    // o(n)
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
         
-        return Math.abs(calcDepth(root.left)  - calcDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        return checkDepth(root) != -1;
     }
     
-    public int calcDepth(TreeNode root) {
+    public int checkDepth(TreeNode root) {
         if (root == null) return 0;
         
-        return Math.max(calcDepth(root.left) + 1, calcDepth(root.right) + 1);
+        int left = checkDepth(root.left);
+        int right = checkDepth(root.right);
+        // check left and right
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        
+        return Math.max(left, right) + 1;
     }
 }
