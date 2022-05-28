ddf2eb32dd95d4ab821d03d3ee943e3783c95bba
ashburncrash <ashburncrash@gmail.com>
2022-05-28 16:54:39 +0200

    Time: 19 ms (63.99%), Space: 53 MB (27.09%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,50 +1,27 @@
 class KthLargest {
-    // int k = Integer.MIN_VALUE;
-    // PriorityQueue<Integer> queue;
+    int k = Integer.MIN_VALUE;
+    PriorityQueue<Integer> queue;
 
-//     public KthLargest(int k, int[] nums) {
-//         this.k = k;
-//         queue = new PriorityQueue<>();
-        
-//         for(int num : nums) {
-//             add(num);
-//         }
-        
-//     }
-    
-//     public int add(int val) {
-//         // check queue size
-//         if (queue.size() < k) {
-//             queue.add(val);
-//         } else if (queue.peek() < val) {
-//                 queue.remove();
-//                 queue.add(val);
-//             }
-
-//         return queue.peek();
-//     }
-     private static int k;
-    private PriorityQueue<Integer> heap;
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        heap = new PriorityQueue<>();
+        queue = new PriorityQueue<>();
         
-        for (int num: nums) {
-            heap.offer(num);
+        for(int num : nums) {
+            add(num);
         }
         
-        while (heap.size() > k) {
-            heap.poll();
-        }
     }
     
     public int add(int val) {
-        heap.offer(val);
-        if (heap.size() > k) {
-            heap.poll();
-        }
+        // check queue size
+        if (queue.size() < k) {
+            queue.add(val);
+        } else if (queue.peek() < val) {
+                queue.remove();
+                queue.add(val);
+            }
 
-        return heap.peek();
+        return queue.peek();
     }
 }
 
