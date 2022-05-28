fb8ca7bfe159e8faccad0738343e27e8b4fe0c6b
ashburncrash <ashburncrash@gmail.com>
2022-05-28 16:50:03 +0200

    Time: 25 ms (33.62%), Space: 52.2 MB (68.99%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,54 @@
+class KthLargest {
+    int k = Integer.MIN_VALUE;
+    PriorityQueue<Integer> queue;
+    ArrayList<Integer> backQueue;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        queue = new PriorityQueue<>(); 
+        backQueue = new ArrayList<>();
+        
+        for(int num : nums) {
+            add(num);
+        }
+        
+    }
+    
+//     ["KthLargest","add","add","add","add","add"]
+// [[3,[2,4,5,8]],[3],[5],[10],[9],[4]]
+    
+//     [4,5,8]
+    public int add(int val) {
+        // check queue size
+        if (queue.size() < k) {
+            queue.add(val);
+        } else if (queue.peek() < val) {
+                queue.remove();
+                queue.add(val);
+            }
+        // if min / first < val -> remove and add
+        
+        
+        // return first 
+        
+//         int temp = k;
+//         while (--temp > 0) { 
+//             backQueue.add(queue.remove());
+//         }
+        
+//         int tempResult = queue.peek();
+        
+//         queue.addAll(backQueue);
+//         backQueue.clear();
+        
+//         return tempResult;
+        return queue.peek();
+    }
+}
+
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
