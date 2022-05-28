efeab0271f0e0361c263663183faa94ff66b4fdc
ashburncrash <ashburncrash@gmail.com>
2022-05-28 16:54:23 +0200

    Time: 39 ms (10.75%), Space: 52.5 MB (59.18%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,46 +1,50 @@
 class KthLargest {
-    int k = Integer.MIN_VALUE;
-    PriorityQueue<Integer> queue;
+    // int k = Integer.MIN_VALUE;
+    // PriorityQueue<Integer> queue;
 
-    public KthLargest(int k, int[] nums) {
-        this.k = k;
-        queue = new PriorityQueue<>();
+//     public KthLargest(int k, int[] nums) {
+//         this.k = k;
+//         queue = new PriorityQueue<>();
         
-        for(int num : nums) {
-            add(num);
-        }
-        
-    }
-    
-//     ["KthLargest","add","add","add","add","add"]
-// [[3,[2,4,5,8]],[3],[5],[10],[9],[4]]
-    
-//     [4,5,8]
-    public int add(int val) {
-        // check queue size
-        if (queue.size() < k) {
-            queue.add(val);
-        } else if (queue.peek() < val) {
-                queue.remove();
-                queue.add(val);
-            }
-        // if min / first < val -> remove and add
-        
-        
-        // return first 
-        
-//         int temp = k;
-//         while (--temp > 0) { 
-//             backQueue.add(queue.remove());
+//         for(int num : nums) {
+//             add(num);
 //         }
         
-//         int tempResult = queue.peek();
+//     }
+    
+//     public int add(int val) {
+//         // check queue size
+//         if (queue.size() < k) {
+//             queue.add(val);
+//         } else if (queue.peek() < val) {
+//                 queue.remove();
+//                 queue.add(val);
+//             }
+
+//         return queue.peek();
+//     }
+     private static int k;
+    private PriorityQueue<Integer> heap;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        heap = new PriorityQueue<>();
         
-//         queue.addAll(backQueue);
-//         backQueue.clear();
+        for (int num: nums) {
+            heap.offer(num);
+        }
         
-//         return tempResult;
-        return queue.peek();
+        while (heap.size() > k) {
+            heap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if (heap.size() > k) {
+            heap.poll();
+        }
+
+        return heap.peek();
     }
 }
 
