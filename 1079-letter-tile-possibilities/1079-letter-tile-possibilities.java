b592e52914114ae684d16c735b9fc69e0a4a65e4
ashburncrash <ashburncrash@gmail.com>
2024-03-22 14:28:07 +0900

    Time: 2 ms (94.19%), Space: 41.3 MB (91.13%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,13 +1,17 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
-        // approach1: hashSet
-        Set<String> result = new HashSet<>();
         boolean[] visited = new boolean[tiles.length()];
+        // approach1: hashSet
+//         Set<String> result = new HashSet<>();
         
-        dfs_1(tiles, result, visited, new StringBuilder());
+//         dfs_1(tiles, result, visited, new StringBuilder());
         
-        return result.size() - 1;
+//         return result.size() - 1;
+        
         // approach2: counter
+        char[] tilesArray = tiles.toCharArray();
+        Arrays.sort(tilesArray);
+        return dfs_2(tilesArray, visited);
     }
     
     private void dfs_1(String tiles, Set<String> result, boolean[] visited, StringBuilder sb) {
@@ -24,7 +28,19 @@ class Solution {
         }
     }
     
-    private int dfs_2() {
-        return 0;
+    private int dfs_2(char[] tilesArray, boolean[] visited) {
+        int counter = 0;
+        
+        for (int i = 0; i < tilesArray.length; i++) {
+            if (i > 0 && tilesArray[i] == tilesArray[i - 1] && !visited[i - 1]) continue;
+            if (!visited[i]) {
+                visited[i] = true;
+                counter++;
+                counter += dfs_2(tilesArray, visited);
+                visited[i] = false;
+            }
+        }
+        
+        return counter;
     }
 }
